820b948d1b66b69395ab31001fcb4e953be53d22
m8mble <m8mble@vivaldi.net>
2020-11-16 12:57:52 +0100

    Introduce expression include for clients

---
+++ include/clean-test/expression.h
@@ -0,0 +1,14 @@
+// Copyright (c) m8mble 2020.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+#include "expression/And.h"
+#include "expression/Equal.h"
+#include "expression/Lift.h"
+
+namespace clean_test {
+
+using expression::lift;
+
+}
