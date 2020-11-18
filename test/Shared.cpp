08e612fba33389adea99e8a4a1ee62fecc40727e
m8mble <m8mble@vivaldi.net>
2020-11-18 12:05:34 +0100

    Remove foo TU
    ... introduced as library stub.

--- test/Shared.cpp
+++ test/Shared.cpp
@@ -1,8 +1,8 @@
 // Copyright (c) m8mble 2020.
 // SPDX-License-Identifier: BSL-1.0
 
-#include "clean-test/foo.h"
+#include <clean-test/framework/SuiteRegistrationSetup.h>
 
 int main() {
-    foo();
+    clean_test::framework::suite_registration_setup();
 }
