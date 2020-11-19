7a1d5e70ccc865cc0d892e2829a9ed531d768ae2
m8mble <m8mble@vivaldi.net>
2020-11-19 18:32:43 +0100

    Introduce very basic execution Configuration

---
+++ src/execute/Configuration.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) m8mble 2020.
+// SPDX-License-Identifier: BSL-1.0
+
+#include "execute/Configuration.h"
+
+namespace clean_test::execute {
+
+Configuration Configuration::parse(int, char **) {
+    return {};
+}
+
+}
