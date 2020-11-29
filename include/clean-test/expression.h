bb0243f905aa566e775eb9575c03fe9d4c926823
m8mble <m8mble@vivaldi.net>
2020-11-29 14:21:07 +0100

    Introduce expression throw detection

--- include/clean-test/expression.h
+++ include/clean-test/expression.h
@@ -7,11 +7,13 @@
 #include "expression/And.h"
 #include "expression/Equal.h"
 #include "expression/Lift.h"
+#include "expression/Throws.h"
 
 namespace clean_test {
 
 using expression::aborts;
 using expression::debug_aborts;
 using expression::lift;
+using expression::throws;
 
 }
