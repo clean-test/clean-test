43fc7fadcf2c006e5007a303c58eef024976ada4
m8mble <m8mble@vivaldi.net>
2020-11-30 21:47:16 +0100

    Introduce fwd_capture

---
+++ include/clean-test/utils/FwdCapture.h
@@ -0,0 +1,29 @@
+// Copyright (c) m8mble 2020.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+#include <tuple>
+
+namespace clean_test::utils {
+
+/// Helper to perfectly forward universal references into correct lambda-capturable type.
+///
+/// A usual usage pattern looks like:
+/// @code
+/// auto lambda = [fwd = fwd_capture(whatever)] {
+///     auto & [captured] = fwd;
+///     // [...]
+/// };
+/// @endcode
+///
+/// @note Using the seemingly simpler [fwd = std::forward<...>(...)]... captures by value and likely is not desirable.
+template <typename... Args>
+constexpr std::tuple<Args...> fwd_capture(Args &&... args)
+{
+    return {std::forward<Args>(args)...};
+}
+
+}
+
+
