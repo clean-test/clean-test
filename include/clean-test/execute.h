7bfdb2aee77e372882f79506470b3c56f8de7552
m8mble <m8mble@vivaldi.net>
2020-12-03 22:39:28 +0100

    Introduce execute API header

---
+++ include/clean-test/execute.h
@@ -0,0 +1,14 @@
+// Copyright (c) m8mble 2020.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+#include "execute/Configuration.h"
+#include "execute/Main.h"
+
+namespace clean_test {
+
+using Configuration = execute::Configuration;
+using execute::main;
+
+}
