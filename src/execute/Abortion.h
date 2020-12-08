f28874ffbaf97139170903a7acb108310ae412a9
m8mble <m8mble@vivaldi.net>
2020-12-08 20:21:37 +0100

    Introduce Abortion

---
+++ src/execute/Abortion.h
@@ -0,0 +1,11 @@
+// Copyright (c) m8mble 2020.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+namespace clean_test::execute {
+
+/// Tag type thrown when execution of a test-case should be terminated.
+class Abortion final {};
+
+}
