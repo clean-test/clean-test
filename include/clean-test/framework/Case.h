eb6a9b5fecf4624bf67d895face158b728cbf77b
m8mble <m8mble@vivaldi.net>
2020-12-31 16:17:30 +0100

    Hide Case internals
    Use accessors for name and runner. The latter clears the
    runner upon invocation in order to detect errors on
    duplicate calls.

--- include/clean-test/framework/Case.h
+++ include/clean-test/framework/Case.h
@@ -20,6 +20,19 @@ public:
     Case(Name name, Runner runner) noexcept : m_name{std::move(name)}, m_runner{std::move(runner)}
     {}
 
+    Name const & name() const
+    {
+        return m_name;
+    }
+
+    void run(execute::Observer& observer)
+    {
+        m_runner(observer);
+        // make sure the runner is only ever used once.
+        m_runner = nullptr;
+    }
+
+private:
     Name m_name;
     Runner m_runner;
 };
