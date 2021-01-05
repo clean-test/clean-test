5b7a7d0f0d7786ee7e9cbc3f6b3e622ef9938e71
m8mble <m8mble@vivaldi.net>
2021-01-05 21:33:16 +0100

    Introduce OSyncStream

---
+++ src/utils/OSyncStream.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) m8mble 2021.
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef __cpp_lib_syncstream
+
+#include "OSyncStream.h"
+
+#include <unordered_map>
+#include <utility>
+
+namespace clean_test::utils {
+namespace {
+
+/// Manages buffer-specific mutex instances.
+auto & buffer_mutex()
+{
+    static auto singleton = Guarded{std::unordered_map<std::streambuf *, std::mutex>{}};
+    return singleton;
+}
+
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+OSyncStream::OSyncStream(std::ostream & os) :
+    m_destination{(*buffer_mutex().guard())[os.rdbuf()], os.rdbuf()}, m_buffer{}
+{}
+
+OSyncStream::~OSyncStream() noexcept(false)
+{
+    emit();
+}
+
+void OSyncStream::emit()
+{
+    auto const data = std::exchange(m_buffer, {}).str();
+    (**(m_destination.guard())).sputn(data.data(), data.size());
+}
+
+}
+#endif
