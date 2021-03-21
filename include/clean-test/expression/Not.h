eee106b2b6fb3887cca9b9b4777d73aafe796475
m8mble <m8mble@vivaldi.net>
2021-03-21 23:05:04 +0100

    Migrate Not to evaluation paradigm

--- include/clean-test/expression/Not.h
+++ include/clean-test/expression/Not.h
@@ -9,37 +9,83 @@
 
 namespace clean_test::expression {
 
+template <BasicExpression T>
+class NotEvaluation;
+
 /// Unary negation of clauses.
 template <BasicExpression T>
 class Not : public Base {
 public:
-    constexpr Not(T const & expression) : m_expression{expression}
+    using Value = decltype(not std::declval<typename T::Value>());
+    using Evaluation = NotEvaluation<T>;
+
+    constexpr explicit Not(T const & expression) : m_expression{expression}
     {}
+
+    [[nodiscard]] constexpr Value value() const
+    {
+        return evaluation().value();
+    }
 
     [[nodiscard]] constexpr explicit operator bool() const
     {
-        return not evaluate(m_expression);
+        return static_cast<bool>(evaluation());
+    }
+
+    [[nodiscard]] constexpr Evaluation evaluation() const
+    {
+        return Evaluation{*this};
     }
 
     friend std::ostream & operator<<(std::ostream & out, Not const & n)
     {
-        return out << "( not " << n.m_expression << " )";
+        return out << n.evaluation();
     }
 
 private:
+    friend class NotEvaluation<T>;
+
     T const m_expression;
 };
 
-template <typename T> requires (BasicExpression<T>)
+template <BasicExpression T>
 constexpr Not<T> operator not(T const & expression)
 {
-    return Not{expression};
+    return Not<T>{expression};
 }
 
-//template <typename T> requires (BasicExpression<T>)
-//constexpr auto operator not(T const & expression)
-//{
-//    return Not{expression};
-//}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <BasicExpression T>
+class NotEvaluation {
+public:
+    constexpr explicit NotEvaluation(Not<T> const & n);
+
+    [[nodiscard]] constexpr decltype(auto) value() const
+    {
+        return m_value;
+    }
+
+    [[nodiscard]] constexpr explicit operator bool() const
+    {
+        return static_cast<bool>(value());
+    }
+
+    friend std::ostream & operator<<(std::ostream & out, NotEvaluation<T> const & ne)
+    {
+        return out << "( not " << ne.m_evaluation << " )";
+    }
+
+private:
+    typename T::Evaluation m_evaluation;
+    typename Not<T>::Value m_value;
+};
+
+// Implementation //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <BasicExpression T>
+constexpr NotEvaluation<T>::NotEvaluation(Not<T> const & n) :
+    m_evaluation{n.m_expression.evaluation()}, m_value{not m_evaluation.value()}
+{}
 
 }
