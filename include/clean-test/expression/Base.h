590c4f9b67e33defd2b66552ff700df4858e90eb
m8mble <m8mble@vivaldi.net>
2021-03-22 23:37:48 +0100

    Generalize BasicExpression
    ... i.e. loosen requirements to match for more type /
    only require inheritance from `Base`.

--- include/clean-test/expression/Base.h
+++ include/clean-test/expression/Base.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include <type_traits>
-#include <ostream>
+#include <concepts>
 
 namespace clean_test::expression {
 
@@ -12,33 +12,9 @@ class Base {
 };
 
 template <typename T>
-concept Printable = requires(std::ostream & out, T const & t)
-{
-    { out << t } -> std::convertible_to<std::ostream &>;
-};
-
-template <typename T>
 concept IsBase = std::derived_from<std::remove_cvref_t<T>, Base>;
 
 template <typename T>
-concept Evaluatable = requires(T const & t) { t.value(); } or requires(T const & t) { static_cast<bool>(t); };
-
-template <typename T>
-concept BasicExpression = IsBase<T> and Printable<T> and Evaluatable<T>;
-
-/// Retrieve boolean value of @p t: Convert represented value to @c bool (via @c value iff necessary).
-template <BasicExpression T>
-constexpr bool evaluate(T const & t)
-{
-    auto const & value = [&]() constexpr -> decltype(auto)
-    {
-        if constexpr (requires { t.value(); }) {
-            return t.value();
-        } else {
-            return t;
-        }
-    }();
-    return static_cast<bool>(value);
-}
+concept BasicExpression = IsBase<T>;
 
 }
