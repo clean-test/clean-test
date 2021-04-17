ec2268bc54b1c66e4613b0150a7360543bfa1f41
m8mble <m8mble@vivaldi.net>
2021-04-17 14:11:42 +0200

    Introduce main library
    ... to link against if you are too lazy to write the
    3-line main function.

---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) m8mble 2021.
+// SPDX-License-Identifier: BSL-1.0
+
+#include "execute/Main.h"
+
+int main(int const argc, char ** const argv)
+{
+    return clean_test::execute::main(argc, argv);
+}
