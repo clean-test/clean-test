98fac84679b330bc1bd900c016623fcdc2136db0
m8mble <m8mble@vivaldi.net>
2022-08-01 13:45:50 +0200

    Introduce math utilities

---
+++ include/clean-test/utils/Math.h
@@ -0,0 +1,87 @@
+// Copyright (c) m8mble 2022.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+#include <cmath>
+
+namespace clean_test::utils {
+namespace math_details {
+
+/// Checks that T has a @c std::abs from the standard library.
+///
+/// @note By specification of the standard library, any type with abs-support is always signed.
+template <typename T>
+concept HasStdAbsSupport = requires {
+    { std::abs(std::remove_cvref_t<T>{}) } -> std::same_as<std::remove_cvref_t<T>>;
+};
+
+/// Ensures that a manually implemented version of norm could work (e.g. for float).
+template <typename T>
+concept ManuallyNormable = requires(T t) {
+   requires(std::is_constructible_v<std::remove_cvref_t<T>>); // default constructible for "0".
+   requires(std::totally_ordered<T>);
+   { -t } -> std::convertible_to<std::remove_cvref_t<T>>;
+};
+
+/// Implementation forwarding to @c std::abs.
+template <HasStdAbsSupport T>
+constexpr decltype(auto) norm(T && t) noexcept(noexcept(std::abs(std::forward<T>(t))))
+{
+    return std::abs(std::forward<T>(t));
+}
+
+/// Identity for unsigned builtin types.
+template <typename T>
+constexpr decltype(auto) norm(T && t) noexcept(noexcept(std::forward<T>(t)))
+    requires(std::is_unsigned_v<std::remove_cvref_t<T>>)
+{
+    return std::forward<T>(t);
+}
+
+/// Manually implemented version of abs for types without native support by the standard library.
+///
+/// Implementation assumes that both @c t and @c -t can be represented by @tparam T. Function returns unspecified
+/// results otherwise.
+template <typename T>
+constexpr auto norm(T && t)
+    requires(ManuallyNormable<T> and not HasStdAbsSupport<T> and not std::is_unsigned_v<std::remove_cvref_t<T>>)
+{
+    if (t < std::remove_cvref_t<T>{}) {
+        // For small builtin types, unary minus is computed via promotion to int. Therefore, we need to convert
+        // back to T explicitly.
+        // This cast is problematic for the lowest values of T. The same issue exists for std::abs (with e.g. int) where
+        // this exact setting is specified as UB. We left it as unspecified which at least justifies the conversion.
+        return static_cast<std::remove_cvref_t<T>>(-std::forward<T>(t));
+    }
+    return std::forward<T>(t);
+}
+
+/// Checks for types having a @c norm overload: either by any of the default implementations above or via ADL.
+template <typename T>
+concept Normable = requires(T t) {
+    { norm(t) };
+};
+
+/// Standard CPO according to N4381 for selecting an appropriate @c norm via ADL.
+///
+/// This is implemented as class and not as a lambda in order to forward the exception specification correctly.
+class NormFn {
+public:
+    template <Normable T>
+    constexpr decltype(auto) operator()(T && t) const noexcept(noexcept(norm(std::forward<T>(t))))
+    {
+        return norm(std::forward<T>(t));
+    }
+};
+
+}
+
+/// CPO for calling the correct norm overload.
+constexpr inline auto const & norm = math_details::NormFn{};
+
+/// Return type of invoking @c norm on @tparam T.
+template <typename T>
+using NormValue = decltype(norm(std::declval<T>()));
+
+}
---
+++ test/Math.cpp
@@ -0,0 +1,159 @@
+// Copyright (c) m8mble 2022.
+// SPDX-License-Identifier: BSL-1.0
+
+#include "TestUtilities.h"
+
+#include <clean-test/utils/Math.h>
+
+#include <array>
+#include <vector>
+
+namespace {
+
+namespace ct = clean_test;
+using ct::utils::dynamic_assert;
+
+template <typename T>
+class Wrapper {
+public:
+    auto operator<=>(Wrapper const & other) const noexcept = default;
+
+    constexpr auto operator-() const
+    {
+        if constexpr (std::is_signed_v<std::remove_cvref_t<T>>) {
+            return Wrapper{static_cast<T>(-m_value)};
+        } else {
+            return Wrapper{m_value};
+        }
+    }
+
+    T m_value = {};
+};
+
+template <typename T>
+Wrapper(T&) -> Wrapper<T>;
+
+template <typename T>
+class StaticallyMockable {
+public:
+    static T m_norm;
+
+    auto operator<=>(StaticallyMockable const & other) const noexcept = default;
+};
+
+template <typename T>
+T StaticallyMockable<T>::m_norm = {};
+
+template <typename T>
+constexpr auto norm(StaticallyMockable<T>)
+{
+    return StaticallyMockable<T>::m_norm;
+}
+
+template <typename T>
+void test_floaing_point()
+{
+    using Limits = std::numeric_limits<T>;
+    using Mock = StaticallyMockable<T>;
+
+    static_assert(std::is_same_v<T, std::remove_cvref_t<decltype(ct::utils::norm(T{}))>>);
+    static_assert(std::is_same_v<T, decltype(std::abs(T{}))>);
+    static_assert(std::is_same_v<Wrapper<T>, std::remove_cvref_t<decltype(ct::utils::norm(Wrapper<T>{}))>>);
+    static_assert(std::is_same_v<T, decltype(ct::utils::norm(Mock{}))>);
+
+    auto const values = std::vector<T>{
+        0.f, -0.f, 1.f, -1.f, Limits::infinity(), -Limits::infinity(), Limits::max(), Limits::lowest()};
+    for (auto const f : values) {
+        auto const abs = std::fabs(f);
+        dynamic_assert(ct::utils::norm(f) == abs);
+        dynamic_assert(ct::utils::norm(Wrapper{f}).m_value == abs);
+
+        Mock::m_norm = f;
+        dynamic_assert(ct::utils::norm(Mock{}) == f);
+    }
+
+    // Test that the fallback implementation works with a quiet NaN.
+    auto const nan = Limits::quiet_NaN();
+    dynamic_assert(std::isnan(ct::utils::norm(Wrapper<T>{nan}).m_value));
+}
+
+template <typename T>
+void test_integral()
+{
+    auto const abs = [](T t) {
+        if constexpr (std::is_signed_v<T>) {
+            return static_cast<T>(std::abs(t));
+        } else {
+            return t;
+        }
+    };
+
+    using Limits = std::numeric_limits<T>;
+    using Mock = StaticallyMockable<T>;
+
+    static_assert(std::is_same_v<T, std::remove_cvref_t<decltype(ct::utils::norm(T{}))>>);
+    static_assert(std::is_same_v<Wrapper<T>, std::remove_cvref_t<decltype(ct::utils::norm(Wrapper<T>{}))>>);
+    static_assert(std::is_same_v<T, decltype(ct::utils::norm(Mock{}))>);
+
+    auto const values = std::vector<T>{0, 1, 2, Limits::max(), Limits::lowest()};
+    auto const signs = std::array{std::vector<int>{1}, std::vector<int>{1, -1}};
+    for (auto const v : values) {
+        for (auto const sign : signs[std::is_signed_v<T>]) {
+            auto const value = static_cast<T>(sign * v);
+            dynamic_assert(ct::utils::norm(value) == abs(value));
+            dynamic_assert(ct::utils::norm(Wrapper{value}).m_value == abs(value));
+
+            Mock::m_norm = value;
+            dynamic_assert(ct::utils::norm(Mock{}) == value);
+        }
+    }
+}
+
+/// Checks that T has a @c std::abs which is constexpr (which it is with C++23).
+///
+/// Also checks that @tparam T is constexpr default constructible. We accept this since @c std::abs is only ever
+/// provided for builtin types which automatically are constexpr anyways.
+template <typename T>
+concept HasConstexprStdAbsSupport = requires {
+    { std::bool_constant<(std::abs(std::remove_cvref_t<T>{}), true)>{} } -> std::same_as<std::true_type>;
+};
+
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static_assert(ct::utils::math_details::HasStdAbsSupport<int>);
+static_assert(ct::utils::math_details::HasStdAbsSupport<const int&>);
+static_assert(ct::utils::math_details::HasStdAbsSupport<int&>);
+static_assert(ct::utils::math_details::HasStdAbsSupport<float>);
+static_assert(not ct::utils::math_details::HasStdAbsSupport<char>);
+
+static_assert(ct::utils::math_details::ManuallyNormable<int>);
+static_assert(ct::utils::math_details::ManuallyNormable<float &>);
+static_assert(ct::utils::math_details::ManuallyNormable<const char&>);
+static_assert(ct::utils::math_details::ManuallyNormable<Wrapper<int>>);
+static_assert(not ct::utils::math_details::ManuallyNormable<std::string_view>);
+
+static_assert(ct::utils::math_details::Normable<float>);
+static_assert(ct::utils::math_details::Normable<int &>);
+static_assert(ct::utils::math_details::Normable<const char&>);
+static_assert(not ct::utils::math_details::Normable<std::string_view>);
+
+static_assert(not HasConstexprStdAbsSupport<float> or ct::utils::norm(-0.f) == 0.f);
+static_assert(ct::utils::norm(1u) == 1u);
+static_assert(not HasConstexprStdAbsSupport<int> or ct::utils::norm(-2) == 2);
+
+int main()
+{
+    test_floaing_point<float>();
+    test_floaing_point<double>();
+    test_floaing_point<long double>();
+
+    test_integral<int>();
+    test_integral<long>();
+    test_integral<long long>();
+    test_integral<char>();
+
+    test_integral<unsigned char>();
+    test_integral<unsigned int>();
+    test_integral<unsigned long>();
+}
