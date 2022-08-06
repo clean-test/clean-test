55b4c8bcb15ef3182e8f183a6bd8b3e66ec7f323
m8mble <m8mble@vivaldi.net>
2022-08-06 23:26:54 +0200

    Introduce type aliases for lifted expressions

--- include/clean-test/expression/Lift.h
+++ include/clean-test/expression/Lift.h
@@ -6,6 +6,8 @@
 #include "Base.h"
 #include "Clause.h"
 
+#include <clean-test/utils/RemoveRvalueReference.h>
+
 namespace clean_test::expression {
 
 /// Lift value @p t to a unit value of @c Clause<T> (iff needed).
@@ -21,5 +23,12 @@ constexpr decltype(auto) lift(T && t) noexcept
     }
 }
 
+/// Type returned from lifting @tparam T.
+template <typename T>
+using Lift = utils::RemoveRvalueReference<decltype(lift(std::declval<T>()))>;
+
+/// Value type provided by evaluating a lifted expression of @tparam T.
+template <typename T>
+using LiftValue = typename std::remove_cvref_t<Lift<T>>::Value;
 
 }
