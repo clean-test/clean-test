2d055f3741eaa7d24d7f75e3c1ddd4138af566d8
m8mble <m8mble@vivaldi.net>
2022-08-28 08:52:39 +0200

    Introduce Pack

---
+++ include/clean-test/utils/Pack.h
@@ -0,0 +1,14 @@
+// Copyright (c) m8mble 2022.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+namespace clean_test::utils {
+
+/// A simple way to pass a collection of types @tparam Args for meta programming.
+template <typename... Args>
+class Pack {};
+
+}
+
+
