cfd637c8b1880419c4febcb5a815349cf1338bfd
m8mble <m8mble@vivaldi.net>
2022-08-28 08:53:45 +0200

    Introduce Tuple concept

---
+++ include/clean-test/utils/Tuple.h
@@ -0,0 +1,36 @@
+// Copyright (c) m8mble 2022.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+#include <tuple>
+#include <utility>
+
+namespace clean_test::utils {
+namespace tuple_details {
+
+template<class T, std::size_t n>
+concept TupleElement =
+    requires(T t) {
+        typename std::tuple_element_t<n, std::remove_const_t<T>>;
+        { get<n>(t) } -> std::convertible_to<const std::tuple_element_t<n, T>&>;
+    };
+
+template <typename T>
+constexpr auto all_tuple_elements
+    = []<std::size_t... idx>(std::index_sequence<idx...>) constexpr
+    {
+        return (TupleElement<T, idx> && ...);
+    }(std::make_index_sequence<std::tuple_size_v<T>>());
+
+}
+
+/// Detects tuple-like objects, i.e. types providing expected functionality in terms of std::tuple_element and _size.
+template <typename T>
+concept Tuple
+    = requires(T t) {
+          { std::tuple_size<T>::value } -> std::convertible_to<std::size_t>;
+          requires tuple_details::all_tuple_elements<T>;
+      };
+
+}
--- test/Utils.cpp
+++ test/Utils.cpp
@@ -4,6 +4,7 @@
 #include "TestUtilities.h"
 
 #include <utils/Printable.h>
+#include <utils/Tuple.h>
 
 #include <vector>
 
@@ -18,6 +19,14 @@ static_assert(ct::utils::Printable<int &>);
 static_assert(ct::utils::Printable<int const &>);
 static_assert(ct::utils::Printable<int &&>);
 static_assert(not ct::utils::Printable<std::vector<int>>);
+
+
+// Tuple ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct Aggregate { int i; };
+static_assert(ct::utils::Tuple<std::tuple<int&>>);
+static_assert(not ct::utils::Tuple<int>);
+static_assert(not ct::utils::Tuple<Aggregate>); // supports structured bindings, but not tuple-style metaprogramming.
 
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
