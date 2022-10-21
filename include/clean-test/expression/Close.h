18f38a74a08bcca488d697a465f9024f3938306c
m8mble <m8mble@vivaldi.net>
2022-10-21 21:33:26 +0200

    Avoid ADL when measuring distance in is_close

--- include/clean-test/expression/Close.h
+++ include/clean-test/expression/Close.h
@@ -16,7 +16,9 @@ namespace clean_test::expression {
 template <typename L, typename R>
 constexpr auto is_close(L && lhs, R && rhs)
 {
-    auto dist = distance(std::forward<L>(lhs), std::forward<R>(rhs));
+    // Intentionally avoid ADL for distance, since we always want to use clean_test::distance independent
+    // of the namespaces of lhs / rhs (e.g. avoid std::distance).
+    auto dist = ::clean_test::expression::distance(std::forward<L>(lhs), std::forward<R>(rhs));
     return std::move(dist) <= tolerance<typename decltype(dist)::NormValue>();
 }
 
